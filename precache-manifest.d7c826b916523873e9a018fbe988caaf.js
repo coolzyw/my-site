3a59de3116a29c42129913a0b011e3c4fdf8cc6a
Yiwei Zhang <happyzyw@umich.edu>
2020-03-23 00:45:03 -0500

    Updates

---
+++ precache-manifest.d7c826b916523873e9a018fbe988caaf.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43ee12ff1488b17ba1aa8466da14e257",
+    "url": "/my-site/index.html"
+  },
+  {
+    "revision": "afddba1e8971024c245e",
+    "url": "/my-site/static/css/main.90df7021.chunk.css"
+  },
+  {
+    "revision": "ad791106cc1830893b21",
+    "url": "/my-site/static/js/2.f859b2e5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-site/static/js/2.f859b2e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afddba1e8971024c245e",
+    "url": "/my-site/static/js/main.c44de106.chunk.js"
+  },
+  {
+    "revision": "db3c1403ad9e58f3d2cb",
+    "url": "/my-site/static/js/runtime-main.b91de792.js"
+  },
+  {
+    "revision": "743e4e02c53b67733b3427f9d0cfde3a",
+    "url": "/my-site/static/media/Figure_1.743e4e02.png"
+  },
+  {
+    "revision": "0e3e73236c49b349ee7002161655ad80",
+    "url": "/my-site/static/media/Figure_2.0e3e7323.png"
+  },
+  {
+    "revision": "40e3a9e546d528c0189a96a9dc1dfbb9",
+    "url": "/my-site/static/media/Figure_3.40e3a9e5.png"
+  },
+  {
+    "revision": "76907770728ed065bb9f1c66de482018",
+    "url": "/my-site/static/media/GPU_driver.76907770.mov"
+  },
+  {
+    "revision": "a1c461e0c48953cd51b7e0705e27c3bc",
+    "url": "/my-site/static/media/NU.a1c461e0.png"
+  },
+  {
+    "revision": "7b33002eebfd753abfa63ab0a2dce20d",
+    "url": "/my-site/static/media/UI_1.7b33002e.png"
+  },
+  {
+    "revision": "f9aa48249edb97c196b1953a4d3eeea0",
+    "url": "/my-site/static/media/UI_2.f9aa4824.png"
+  },
+  {
+    "revision": "98eefa0c57b23bc9f081bee25e70bfd7",
+    "url": "/my-site/static/media/fb.98eefa0c.png"
+  },
+  {
+    "revision": "3f15c460b5a3bac9178620db962be205",
+    "url": "/my-site/static/media/graphics1.3f15c460.mov"
+  },
+  {
+    "revision": "7825ac1505365ece4d10cd3f21186f6b",
+    "url": "/my-site/static/media/graphics2.7825ac15.mov"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-site/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0cdd8b4ead9ac5c0199e11071dee881e",
+    "url": "/my-site/static/media/profile.0cdd8b4e.JPG"
+  },
+  {
+    "revision": "6a6b4f0eb9dc198879d3dbca70064cd0",
+    "url": "/my-site/static/media/sjtu.6a6b4f0e.png"
+  },
+  {
+    "revision": "9dc45cdfa996b5e693cdbe3c3ea6501a",
+    "url": "/my-site/static/media/umich.9dc45cdf.png"
+  }
+]);
